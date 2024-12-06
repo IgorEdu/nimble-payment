d345103c5b190248f2efc782a9b45df99b269ea8
Igor <igoreduardo20101@gmail.com>
2024-12-06 09:31:55 -0300

    test: test login on authentication service

---
+++ src/test/java/com/nimble/payment/PaymentGaytwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimble.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGaytwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/nimble/payment/service/AuthenticationServiceTest.java
@@ -0,0 +1,58 @@
+package com.nimble.payment.service;
+
+import com.nimble.payment.domain.User;
+import com.nimble.payment.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AuthenticationServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private User user;
+
+    @InjectMocks
+    private AuthenticationService authenticationService;
+
+    @Test
+    void loadUserByUsername_ShouldReturnUserDetails_WhenUserExists() {
+        // Arrange
+        String testUsername = "12345678900";
+
+        when(userRepository.findByCpf(testUsername)).thenReturn((User) user);
+
+        authenticationService.userRepository = userRepository;
+
+        // Act
+        UserDetails userDetails = authenticationService.loadUserByUsername(testUsername);
+
+        // Assert
+        assertNotNull(userDetails);
+        assertEquals(user, userDetails);
+        verify(userRepository, times(1)).findByCpf(testUsername);
+    }
+
+    @Test
+    void loadUserByUsername_ShouldThrowUsernameNotFoundException_WhenUserDoesNotExist() {
+        // Arrange
+        String testUsername = "12345678900";
+
+        when(userRepository.findByCpf(testUsername)).thenReturn(null);
+        authenticationService.userRepository = userRepository;
+
+        // Act & Assert
+        assertNull(authenticationService.loadUserByUsername(testUsername));
+
+        verify(userRepository, times(1)).findByCpf(testUsername);
+    }
+}
