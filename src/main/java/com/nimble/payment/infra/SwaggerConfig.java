099ae44ccac8a21c67b9b7ea32f62040d6bdfda6
Igor <igoreduardo20101@gmail.com>
2024-12-06 11:03:11 -0300

    chore: includes component of token on swagger ui

---
+++ src/main/java/com/nimble/payment/infra/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.nimble.payment.infra;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Nimble Payment API")
+                        .description("API for Nimble Payment")
+                        .version("1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"));
+    }
+}
