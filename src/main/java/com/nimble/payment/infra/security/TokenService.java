9e82c9601deff56ad2928e1cba572341fe04f65b
Igor <igoreduardo20101@gmail.com>
2024-12-06 23:19:40 -0300

    move authentication and user model to new package "domain/user"

--- src/main/java/com/nimble/payment/infra/security/TokenService.java
+++ src/main/java/com/nimble/payment/infra/security/TokenService.java
@@ -3,7 +3,7 @@ package com.nimble.payment.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
-import com.nimble.payment.domain.User;
+import com.nimble.payment.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
